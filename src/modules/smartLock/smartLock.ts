77af53f8fd4105bd013697515a8be38b79b1cd32
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-10-18 07:22:08 +0200

    add validation and emit create reservation event

--- src/modules/smartLock/smartLock.ts
+++ src/modules/smartLock/smartLock.ts
@@ -1,4 +1,5 @@
 import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { eventEmitter } from '../../events/eventEmitter';
 import { prisma } from '../../prismaClient';
 import {
   CancelReservationInput,
@@ -15,9 +16,36 @@ export class SmartLockResolver {
 
   @Mutation(() => Reservation)
   async createReservation(@Arg('data') data: ReservationCreateInput) {
+    // make sure unit exists
+    const unit = await prisma.unit.findFirst({
+      where: {
+        id: data.unitId,
+      },
+    });
+    if (!unit) {
+      throw new Error('Unit does not exist');
+    }
+
+    // make sure the unit is not already reserved for the given dates
+    const isReserved = await prisma.reservation.findFirst({
+      where: {
+        unitId: data.unitId,
+        checkIn: {
+          lte: data.checkIn,
+        },
+        checkOut: {
+          gte: data.checkIn,
+        },
+      },
+    });
+    if (isReserved) {
+      throw new Error('Unit is already reserved for the given dates');
+    }
+
     const newReservation = await prisma.reservation.create({
       data,
     });
+    eventEmitter.emit('reservationCreated', newReservation);
     return {
       ...newReservation,
       id: Number(newReservation.id), // to solve prisma returning BigInt type 9n instead of bigint 9
@@ -39,6 +67,7 @@ export class SmartLockResolver {
         unitId: rest.unitId,
       },
     });
+    eventEmitter.emit('reservationUpdated', updatedReservation);
     return {
       ...updatedReservation,
       id: Number(updatedReservation.id),
@@ -48,6 +77,16 @@ export class SmartLockResolver {
 
   @Mutation(() => Reservation)
   async cancelReservation(@Arg('data') data: CancelReservationInput) {
+    // make sure reservation exists
+    const reservation = await prisma.reservation.findFirst({
+      where: {
+        id: data.reservationId,
+      },
+    });
+    if (!reservation) {
+      throw new Error('Reservation does not exist');
+    }
+
     const canceledReservation = await prisma.reservation.update({
       where: {
         id: data.reservationId,
@@ -56,6 +95,8 @@ export class SmartLockResolver {
         isCancelled: true,
       },
     });
+    eventEmitter.emit('reservationCanceled', canceledReservation);
+
     return {
       ...canceledReservation,
       id: Number(canceledReservation.id),
